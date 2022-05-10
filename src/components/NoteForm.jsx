53ec955ffbc16a51029fdf66ef05cb8151763ac9
A-IdrissiHamza <a.idrissihamza@gmail.com>
2022-05-10 23:16:27 +0200

    focus on input when map marked

--- src/components/NoteForm.jsx
+++ src/components/NoteForm.jsx
@@ -4,18 +4,27 @@ import { faPlusCircle } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
 import { GlobalContext } from "../context/GlobalContext";
 import { useContext } from "react";
+import { useRef } from "react";
+import { useEffect } from "react";
 const NoteForm = ({ submit }) => {
   const context = useContext(GlobalContext);
-  const { dispatch,disableInput } = context;
+  const { dispatch, disableInput } = context;
+
+  const inputRef = useRef(null);
+
   const [value, setValue] = useState("");
   const handleSubmit = (e) => {
     e.preventDefault();
     submit(value);
     setValue("");
     dispatch({ type: "DISABLE_FORM" });
-    
+
   };
-  
+  useEffect(() => {
+    if (!disableInput) {
+      inputRef.current.focus();
+    }
+  }, [disableInput]);
   return (
     <form
       onSubmit={handleSubmit}
@@ -29,6 +38,7 @@ const NoteForm = ({ submit }) => {
         onChange={(e) => setValue(e.target.value)}
         required
         disabled={disableInput}
+        ref={inputRef}
       />
       <button className="pr-1 text-2xl hover:text-yellow-500 scale-110  text-yellow-400 active:scale-95 ">
         <FontAwesomeIcon icon={faPlusCircle} />
