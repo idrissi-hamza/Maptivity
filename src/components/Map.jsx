e1c842712da140d31f5a19f957982e3d682d1611
A-IdrissiHamza <a.idrissihamza@gmail.com>
2022-05-10 17:58:23 +0200

    display map where user located

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,34 +1,66 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-const Map = () => {
-  // const[position,setPosition]=useState(null)
-  const position = [51.505, -0.09];
-  // if (navigator.geolocation)
-  //   navigator.geolocation.getCurrentPosition(
-  //     function (position) {
-  //       const { latitude, longitude } = position.coords;
-  //       // setPosition
-  //       console.log(latitude, longitude);
-  //     },
-  //     function (err) {
-  //       console.log(err.message);
-  //     }
-  //   );
+import { icon } from "leaflet";
+const ICON = icon({
+  iconUrl: "/marker.png",
+  iconSize: [32, 32],
+});
 
+const Map = () => {
+  const ZOOM = 13;
+  const [location, setLocation] = useState({
+    loading: true,
+    coordinates: { lat: "", lng: "" },
+  });
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        function (position) {
+          setLocation({
+            loading: false,
+            coordinates: {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+            },
+          });
+        },
+        function (err) {
+          // alert(err.message);
+          setLocation({
+            loading: false,
+            err,
+          });
+        }
+      );
+    }
+  }, []);
   return (
     <div className="w-full">
-      <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={position}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
+      {!location.loading &&
+        !location.err && (
+          <MapContainer
+            center={[location.coordinates.lat, location.coordinates.lng]}
+            zoom={ZOOM}
+            scrollWheelZoom={false}
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            {/* {!location.loading && !location.err && (
+          <Marker
+            icon={ICON}
+            position={[location.coordinates.lat, location.coordinates.lng]}
+          >
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        )} */}
+          </MapContainer>
+        )}
     </div>
   );
 };
